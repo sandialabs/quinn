e18787ddd8a2e3e0a146b9fabcb0296fecbb8b48
ksargsyan <ksargsy@sandia.gov>
2023-08-12 22:41:46 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,10 +38,23 @@ release = ''
 #
 # needs_sphinx = '1.0'
 
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.inheritance_diagram',
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.bibtex',
+#    'sphinx.ext.pngmath'
+]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -75,8 +88,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
-# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
